c4710ff8e37bb45c12b12818f8ced59161200ca2
RyanAngJY <ryanang.jy@gmail.com>
2018-03-06 19:43:54 +0800

    Added Url support for SampleDataUtil.java

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -10,6 +10,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Url;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.tag.Tag;
 
@@ -20,23 +21,23 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
+                new Url("https://www.allrecipes.com/recipe/73634/colleens-slow-cooker-jambalaya/"),
+                new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
+                new Url("https://www.allrecipes.com/recipe/8722/mexican-chicken-i/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%203"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getTagSet("colleagues", "friends")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
+                new Url("https://www.allrecipes.com/recipe/11901/to-die-for-fettuccini-alfredo/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%206"),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getTagSet("neighbours")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
+                new Url("https://www.allrecipes.com/recipe/229110/savory-beef-stir-fry/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%2014"),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getTagSet("family")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
+                new Url("https://www.allrecipes.com/recipe/222615/scrambled-egg-brunch-bread/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%2020"),
+                new Address("Blk 47 Tampines Street 20, #17-35"), getTagSet("classmates")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                new Url("https://www.allrecipes.com/recipe/15917/fudge-truffle-cheesecake/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%2022"),
+                new Address("Blk 45 Aljunied Street 85, #11-31"), getTagSet("colleagues"))
         };
     }
 
