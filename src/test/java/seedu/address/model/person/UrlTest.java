1dcdbf516d4fa32473fa6baa94ca4fca04d727f8
RyanAngJY <ryanang.jy@gmail.com>
2018-03-07 14:14:47 +0800

    Updated XmlAdaptedPersonText.java to include URL. Created UrlTest.java

---
+++ src/test/java/seedu/address/model/person/UrlTest.java
@@ -0,0 +1,22 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class UrlTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Url(null));
+    }
+
+    @Test
+    public void constructor_invalidEmail_throwsIllegalArgumentException() {
+        String invalidUrl = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Email(invalidUrl));
+    }
+
+}
