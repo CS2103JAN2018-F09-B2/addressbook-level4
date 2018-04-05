99ddca6b08ee2b25d0ed59f7af7d84dc563bfcfb
RyanAngJY <ryanang.jy@gmail.com>
2018-04-05 18:35:40 +0800

    Minor refactoring of UiUtil

--- src/main/java/seedu/recipe/ui/RecipeCard.java
+++ src/main/java/seedu/recipe/ui/RecipeCard.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.recipe.model.recipe.Recipe;
+import seedu.recipe.ui.util.UiUtil;
 
 /**
  * An UI component that displays information of a {@code Recipe}.
